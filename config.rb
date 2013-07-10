978eb9453f81c544f1b2a351274b221b794c8110
Nic Haynes <nic@nicinabox.com>
2013-07-10 10:51:59 -0500

    Setup baseline

---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'compass-normalize'
+require 'modular-scale'
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+# output_style = :compressed
+# line_comments = false
